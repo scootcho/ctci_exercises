f4c24944109720e2a877008e860447a61b3aea44
Scott Yu <scott.tj.yu@gmail.com>
2016-01-23 16:29:42 -0800

    update jukebox

--- 8.3_jukebox_player.rb
+++ 8.3_jukebox_player.rb
@@ -33,7 +33,7 @@
 # The Play list manages the current and next songs to play. It is essentially a wrapper class for a queue and offers some additional methods for convenience.
 
 class Jukebox
-  attr_accessor :play_list, :available_songs, :song_playing
+  attr_reader :play_list, :available_songs, :song_playing
 
   Songs = [["ACDC", "3:32", "Born to be Wild"],["Red Hot Chili Peppers", "3:37", "By the Way"], ["Adele", "3:30", "Chasing Pavements"]]
 
@@ -46,15 +46,44 @@ class Jukebox
       artist, time, title = song
       @available_songs[index + 1] = Song.new(artist, time, title)
     end
+
     player_dashboard
   end
 
+  def menu(option)
+    case option
+    when "d"
+      puts "\n"
+      puts "========================== Jukebox Player ============================"
+      puts "(a) add credit | (b) browse songs | (p) player | (x) exit".center(70)
+      puts "======================================================================"
+      puts "available credit: #{@credit}".rjust(70)
+      puts "\n"
+    when "b"
+      puts "\n"
+      puts "========================== Jukebox Player ============================"
+      puts "(a) add credit | (m) return to menu | (p) player | (x) exit".center(70)
+      puts "======================================================================"
+      puts "available credit: #{@credit}".rjust(70)
+      puts "\n"
+    when "p"
+      puts "\n"
+      puts "========================== Jukebox Player ============================"
+      puts "(a) add credit | (m) return to menu | (p) player | (x) exit".center(70)
+      puts "------------------------- player control -----------------------------"
+      puts "(p) play song | (n) next song | (l) show play list | (d) delete song"
+      puts "======================================================================"
+      puts "available credit: #{@credit}".rjust(70)
+      puts "\n"
+      puts "currently playing: #{@song_playing.artist} - #{@song_playing.title}".ljust(70) if @song_playing != nil
+      puts "\n"
+    end
+  end
+
   def player_dashboard
+    menu("d")
+    puts "what would you like to do?".center(70)
     puts "\n"
-    puts "(a) add credit | (b) browse songs | (p) player | (x) exit"
-    puts "======================================================================"
-    puts "what would you like to do?             | available credit: #{@credit}"
-    puts "\n\n"
 
     option = gets.chomp
     case option
@@ -65,32 +94,36 @@ class Jukebox
     when "p"
       music_player
     when "x"
-      puts "Good bye!"
+      puts "\n"
+      puts "========== Good bye! ===========".center(70)
       return
     else
-      puts "please select a valid option"
+      puts "\n"
+      puts "please select a valid option".center(70)
       player_dashboard
     end
   end
   
   def purchase_credit
-    puts "Enter credits you would like to add - 1 credit for 1 song"
+    puts "Enter credits you would like to add - 1 credit for 1 song".center(70)
+    puts "\n"
     num = gets.chomp.to_i
-    puts "Please enter a valid number greater than 1" if num == 0
-    @credit += num
-    puts "** credit added! **"
-    player_dashboard
+    if num == 0
+      puts "\n"
+      puts "*** Please enter a valid number greater than 1 ***" if num == 0
+      purchase_credit
+    else
+      @credit += num
+      puts "*** credit added! ***".center(70)
+      player_dashboard
+    end
   end
 
   def browse_songs
-    puts "\n"
-    puts "(a) add credit | (m) return to menu | (p) player | (x) exit"
-    puts "======================================================================"
-    puts "Enter song number or other options       | available credit: #{@credit}"
-    puts "\n\n"
-
+    menu("b")
     display_songs
-
+    puts "\n"
+    puts "*** Select song number to add to play list ***".center(70)
     option = gets.chomp
     add_song(option.to_i) if option.to_i > 0 
 
@@ -102,10 +135,12 @@ class Jukebox
     when "p"
       music_player
     when "x"
-      puts "Good bye!"
+      puts "\n"
+      puts "========== Good bye! ===========".center(70)
       return
     else
-      puts "please select a valid option"
+      puts "\n"
+      puts "please select a valid option".center(70)
       browse_songs
     end
   end
@@ -118,14 +153,17 @@ class Jukebox
 
   def display_play_list
     if @play_list.empty?
-      puts "There are no songs in the play list"
+      puts "*** There are no songs in the play list ***".center(70)
+      music_player
     else
-      puts "========= Play List =========="
+      puts "*********** Play List ************".center(70)
       puts "\n"
       @play_list.each.with_index do |song, index|
         puts "(#{index + 1}) #{song.artist}, #{song.time}, #{song.title}"
       end
       puts "\n"
+      puts "*********** End List ************".center(70)
+      music_player
     end
   end
 
@@ -133,30 +171,23 @@ class Jukebox
     if @available_songs[key]
       song =  @available_songs[key]
     else
-      puts "please enter a valid song number"
+      puts "*** please enter a valid song number ***".center(70)
       browse_songs
     end
 
     if check_credit
-      puts "*** song added: #{song.artist} - #{song.title} - #{song.time} ***"
+      puts "*** song added: #{song.artist} - #{song.title} - #{song.time} ***".center(70)
       @credit -= 1
       @play_list << song
       browse_songs
     else
-      puts "You have no credit left. Please add more"
+      puts "*** You have no credit. Please add more ***".center(70)
       browse_songs
     end
   end
 
   def music_player
-    puts "================ JukeBox Player ====================="
-    puts "\n"
-    puts "(a) add credit | (m) return to menu | (x) exit"
-    puts "======================================================================"
-    puts "(p) play song | (n) next song | (l) show play list | (d) delete song"
-    puts "currently playing: #{@song_playing.artist} - #{@song_playing.title}" if @song_playing != nil
-    puts "\n\n"
-    display_play_list
+    menu("p")
 
     option = gets.chomp
     add_song(option.to_i) if option.to_i > 0 
@@ -175,12 +206,14 @@ class Jukebox
     when "d"
       delete_song
     when "x"
-      puts "Good bye!"
+      puts "\n"
+      puts "========== Good bye! ===========".center(70)
       return
     else
-      puts "please select a valid option"
+      puts "\n"
+      puts "please select a valid option".center(70)
+      music_player
     end
-
   end
 
   def check_credit
@@ -188,11 +221,12 @@ class Jukebox
   end
 
   def delete_song
-    puts "pick song number to delete"
+    puts "=== DELETE SONG ===".center(70)
+    puts "pick song number to delete".center(70)
     num = gets.chomp
     index = num.to_i - 1
     @play_list.delete_at(index)
-    puts "song deleted and refunded credit!"
+    puts "*** song deleted and refunded credit! ***".center(70)
     @credit += 1  
     music_player
   end
@@ -227,31 +261,3 @@ end
 
 # ### Sample Output:
 player = Jukebox.new
-
-# ...
-# (a) add credit | (m) return to menu | (p) player | (x) exit
-# ======================================================================
-# Enter song number or other options       | available credit: 5
-# 
-# 
-# (1) ACDC - Born to be Wild - 3:32
-# (2) Red Hot Chili Peppers - By the Way - 3:37
-# (3) Adele - Chasing Pavements - 3:30
-# 1
-# *** song added: ACDC - Born to be Wild - 3:32 ***
-# 
-# ...
-# ================ JukeBox Player =====================
-# 
-# (a) add credit | (m) return to menu | (x) exit
-# ======================================================================
-# (p) play song | (n) next song | (l) show play list | (d) delete song
-
-# currently playing: ACDC - Born to be Wild
-# 
-# 
-# ========= Play List ==========
-# 
-# (1) ACDC, 3:32, Born to be Wild
-# (2) Red Hot Chili Peppers, 3:37, By the Way
-
